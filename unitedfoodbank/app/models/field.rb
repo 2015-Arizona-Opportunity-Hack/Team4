08f990ce89a6876d1d9d8e09dd23510b9edd6aa1
Tanmay Patil <tanmay@Tanmays-MacBook-Pro.local>
2015-10-10 15:02:30 -0700

    Added new routes and models

---
+++ unitedfoodbank/app/models/field.rb
@@ -0,0 +1,15 @@
+class Field
+	include Mongoid::Document
+	include Mongoid::Timestamps
+
+ 	field :name, type: String
+	field :type, type: String
+	field :required, type: Boolean
+	field :category, type: String
+	field :tab, type: Integer, default: 1
+	field :is_active, type: Boolean, default: false
+	field :include_in_individual, type: Boolean, default: true
+	field :include_in_corporate, type: Boolean, default: true
+	field :include_in_social, type: Boolean, default: true
+
+end
