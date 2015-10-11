f86b3ab542d5d1ef091d706dbe5e9cee93701d1f
Tanmay Patil <tanmay@Tanmays-MacBook-Pro.local>
2015-10-11 12:53:55 -0700

    Changes for Quality Testing

--- unitedfoodbank/app/controllers/volunteers_controller.rb
+++ unitedfoodbank/app/controllers/volunteers_controller.rb
@@ -11,7 +11,7 @@ class VolunteersController < ActionController::Base
 		respond_to do |format|
 			if @volunteer.save
 				flash[:success_message] = "Volunteer was successfully created."
-				format.html { redirect_to success_volunteer_path(@volunteer) }
+				format.html { redirect_to edit_volunteer_path(@volunteer, {tab: "2"}) }
 				format.json { render json: @volunteer, status: :created}
 			else
 				flash[:error] = "Volunteer already present"
@@ -30,6 +30,47 @@ class VolunteersController < ActionController::Base
 		end
 	end
 
+	def edit
+		@volunteer = Volunteer.find(params[:id])
+		@is_show = false
+		@is_edit = true
+		@fields = [Field.where(is_active: true, tab: 1), Field.where(is_active: true, tab: 2), Field.where(is_active: true, tab: 3)]
+	end
+
+	def update
+		@volunteer = Volunteer.find(params[:id])
+		dyn_attr = @volunteer.dynamic_fields
+		if(params.present? && params[:volunteer].present? && params["volunteer"]["dynamic_fields"].present?)
+			params[:volunteer][:dynamic_fields].each do |x, y|
+				dyn_attr[x] = y
+			end
+		end
+		attributes = volunteer_params
+		if(attributes.empty?)
+			respond_to do |format|
+				format.html { redirect_to success_volunteer_path(@volunteer, {tab: "3"}) }
+			end
+		else
+			attributes["dynamic_fields"] = dyn_attr
+			respond_to do |format|
+				if @volunteer.update_attributes(attributes)
+					if(params[:tab].present? && params[:tab]=="tab2")
+						format.html { redirect_to edit_volunteer_path(@volunteer, {tab: "3"}) }
+					else
+						flash[:success_message] = "Volunteer was successfully updated."
+						format.html { redirect_to success_volunteer_path(@volunteer, {tab: "3"}) }
+						format.json { render json: @volunteer, status: :created}
+					end
+				else
+					flash[:error] = "Volunteer Update Failed"
+					format.html {render action: "new"}
+					format.json { render json: @volunteer.errors.full_messages, status: :unprocessable_entity }
+				end
+			end	
+		end
+		
+	end
+
 	def new
 		if(params[:parent_id].present?)
 			@parent = Volunteer.find(params[:parent_id])
@@ -37,8 +78,9 @@ class VolunteersController < ActionController::Base
 			@parent = nil
 		end
 		@volunteer = Volunteer.new
-		@fields = Field.where(is_active: true)
+		@fields = [Field.where(is_active: true, tab: 1), Field.where(is_active: true, tab: 2), Field.where(is_active: true, tab: 3)]
 		@is_show = false
+		@is_edit = false
 	end
 
 	def show
@@ -48,14 +90,15 @@ class VolunteersController < ActionController::Base
 		elsif(@volunteer.corporate_parent.present?)
 			@parent = @volunteer.corporate_parent
 		end
-		@fields = Field.where(is_active: true)
+		@fields = [Field.where(is_active: true, tab: 1), Field.where(is_active: true, tab: 2), Field.where(is_active: true, tab: 3)]
 		@is_show = true
+		@is_edit = false
 	end
 
 	private
 
 	def volunteer_params
-		params.require(:volunteer).permit!
+		params.require(:volunteer).permit! rescue []
 	end
 	
 end
--- unitedfoodbank/app/models/field.rb
+++ unitedfoodbank/app/models/field.rb
@@ -5,6 +5,7 @@ class Field
  	field :name, type: String
 	field :type, type: String
 	field :data, type: String, default: ''
+	field :tab, type: Integer, default: 1
 	field :required, type: Boolean
 	field :is_active, type: Boolean, default: false
 	field :include_in_individual, type: Boolean, default: true
